03990af47c485ac1c2dcb259fcf99fb19f9c9feb
PrithviSaha <91559696+PrithviSaha@users.noreply.github.com>
2025-07-29 17:01:51 +0530

    Binded assertion file in top.sv

--- src/testbench/top.sv
+++ src/testbench/top.sv
@@ -2,9 +2,9 @@
 `include "alu_pkg.sv"
 `include "alu_design.v"
 `include "alu_if.sv"
-
+`include "alu_assertions.sv"
 module top;
-  // Import all files
+
   import alu_pkg::*;
 
   bit CLK;
@@ -13,10 +13,9 @@ module top;
     forever #5 CLK = ~CLK;
   end
 
-  // Interface instantiation
   alu_if intf(CLK);
 
-  // DUV Instantiation
+  //Instantiate duv
   ALU_DESIGN #(.DW(`WIDTH), .CW(`CMD_WIDTH)) DUV(
         .CLK(CLK),
         .RST(intf.RST),
@@ -36,13 +35,36 @@ module top;
         .ERR(intf.ERR)
   );
 
-  alu_test tb;
+  //Bind assertions
+  bind ALU_DESIGN alu_assertions alu_ass(
+        .CLK(CLK),
+        .RST(intf.RST),
+        .CE(intf.CE),
+        .MODE(intf.MODE),
+        .CMD(intf.CMD),
+        .INP_VALID(intf.INP_VALID),
+        .CIN(intf.CIN),
+        .OPA(intf.OPA),
+        .OPB(intf.OPB),
+        .RES(intf.RES),
+        .COUT(intf.COUT),
+        .OFLOW(intf.OFLOW),
+        .G(intf.G),
+        .L(intf.L),
+        .E(intf.E),
+        .ERR(intf.ERR)
+  );
 
-  // add regression testcases here
+
+  //alu_test tb;
+
+  // Regression test
+  test_regression tb_reg = new(intf.DRV, intf.MON, intf.REF_SB);
 
   initial begin
-    tb = new(intf.DRV, intf.MON, intf.REF_SB);
-    tb.run();
+    //tb = new(intf.DRV, intf.MON, intf.REF_SB);
+    //tb.run();
+    tb_reg.run();
     $finish;
   end
 
